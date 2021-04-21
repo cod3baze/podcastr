b8269689a81c29fb1fe019e1d1bf2cc3ecc17d4e
eliasall3x <elliasallex@gmail.com>
2021-04-21 01:32:04 -0300

    feat: static form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,37 @@
-import { Header } from "../components/Header";
-
-function Home() {
-  return <h1>index</h1>;
+function Home(props) {
+  return (
+    <>
+      <h1>index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </>
+  );
 }
+
+/* vai gerar uma versão estática, html puro, para todos */
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, // vai refazer a cada 8 horas
+  };
+}
+
+// vai carregar de forma estática, sempre que for acessada a HOME
+/*
+  export async function getServerSideProps() {
+    const response = await fetch("http://localhost:3333/episodes");
+    const data = await response.json();
+
+    return {
+      props: {
+        episodes: data,
+      },
+    };
+  }
+*/
 
 export default Home;
